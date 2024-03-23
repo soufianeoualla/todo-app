f842c9d3ee00adcc3421a29ba25293a30b22fc85
soufianeoualla <soufianeoualla0@gmail.com>
2024-03-23 14:10:36 +0000

    edit mail

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmationLink = `${process.env.NEXT_APP_URL}/auth/new-verification?token=${token}`;
+  const confirmationLink = `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "mail@soufian.me",
